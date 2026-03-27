9fcc055b648f11e3614fe91a7db59d31379d6b77
Nigel Todman <veritas@vts-tech.org>
2026-03-27 12:40:40 -0400

    R03.3

--- agentnova/__init__.py
+++ agentnova/__init__.py
@@ -28,7 +28,7 @@ Example Usage:
     agent = Agent(model="qwen2.5:0.5b", soul="/path/to/soul/package")
 """
 
-__version__ = "0.3.1"
+__version__ = "0.3.3"
 __author__ = "VTSTech"
 __status__ = "Alpha"
 
